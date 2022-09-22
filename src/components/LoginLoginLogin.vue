06e35a0d9a08babab3a21b3e4ec2fff7b4b78738
Almir <almir.semic@yahoo.com>
2022-09-22 10:35:24 +0200

    wip

--- src/components/LoginLoginLogin.vue
+++ src/components/LoginLoginLogin.vue
@@ -1,109 +1,86 @@
 <template>
-  <div>
-    <section class="gradient-custom" style="background: #6a11cb; height: 100vh">
-      <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-start h-100">
-          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card bg-dark text-white" style="border-radius: 1rem">
-              <div class="card-body p-5 text-center">
-                <div class="mb-md-5 mt-md-4 pb-5">
-                  <h2 class="fw-bold mb-2 text-uppercase">LOGIN</h2>
-                  <p class="text-white-50 mb-5">
-                    Please enter your username and password!
-                  </p>
-                  <form
-                    @submit.prevent="submitForm"
-                    class="needs-validation"
-                    novalidate
-                  >
-                    <div class="form-outline form-white mb-4">
-                      <label class="form-label"
-                        >Username</label
-                      >
-                      <div>
-                        <input
-                          type="text"
-                          v-model="username"
-                          class="form-control"
-                          required
-                        />
-                        <div v-if="usernameError" style="color: red;">
-                          {{ usernameError }}
-                        </div>
-                      </div>
-                    </div>
-                    <div class="form-outline form-white mb-4">
-                      <label class="form-label"
-                        >Password</label
-                      >
-                      <div>
-                        <input
-                          type="password"
-                          v-model="password"
-                          class="form-control"
-                          required
-                        />
-                        <div v-if="passwordError" style="color: red;">
-                          {{ passwordError }}
-                        </div>
-                      </div>
-                    </div>
-                    <button
-                      class="btn btn-outline-light btn-lg px-5"
-                      type="submit"
-                    >
-                      Login
-                    </button>
-                  </form>
-                </div>
-                <div>
-                  <p class="mb-0">
-                    Don't have an account?
-                    <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  <div class="body">
+    <input type="radio" name="a1" id="chk1" style="display: none;">
+    <input type="radio" name="a1" id="chk2" style="display: none;">
+    <div class="box">
+      <div class="a">
+        <h6 class="title mt-5">TODO LIST</h6>
+        <label for="chk1">Login</label>
+        <label for="chk2" id="sup">Register</label>
       </div>
-    </section>
+      <div class="b">
+        <form action="" class="frm">
+          <br><br><br>
+          <h6 class="title">Register</h6>
+          <input type="text" placeholder="Username">
+          <input type="password" placeholder="Password">
+          <input type="password" placeholder="Password Confirm">
+          <input type="button" value="Sign up" id="btn">
+          <label for="chk1" id="btm">Login Here</label>
+        </form>
+      </div>
+      <div class="c">
+        <form action="" @submit.prevent="submitForm" class="frm">
+          <br><br><br><br>
+          <h6 class="title">Login</h6>
+          <input type="text"  v-model="state.username" placeholder="Username">
+          <div class="input-errors" v-for="error of v$.state.username.$errors" :key="error.$uid">
+          <div class="error-msg">{{ error.$message }}</div>
+          </div> 
+          <input type="password" v-model="state.password" placeholder="Password">
+          <div class="input-errors" v-for="error of v$.state.password.$errors" :key="error.$uid">
+          <div class="error-msg">{{ error.$message }}</div>
+          </div> 
+          <input type="submit" value="Login" id="btn">
+          <label for="chk2" id="btm">Register Here</label>
+        </form>
+      </div>
+      <div class="d"></div>
+      <div class="e"></div>
+    </div>
   </div>
 </template>
 <script>
 import axios from "axios";
 import { mapGetters } from "vuex";
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+import { reactive } from  'vue' // "from '@vue/composition-api'" if you are using Vue 2.x
+
 export default {
-  data() {
+ setup () {
+    const state = reactive({
+      username: '',
+      password: '',
+    })
+    const rules = {
+      username: { required }, // Matches state.firstName
+      password: { required }, // Matches state.lastName
+    }
+    const v$ = useVuelidate(rules, state)
+    return { state, v$ }
+  }, 
+  validations () {
     return {
-      token: null,
-      username: "",
-      password: "",
-      usernameError: '',
-      passwordError: ''
-    };
+      username: { required }, // Matches this.firstName
+      password: { required }, // Matches this.lastName
+    }
   },
+ 
   methods: {
     async submitForm() {
       try {    
         const formData = {
-          username: this.username,
-          password: this.password,
+          username: this.state.username,
+          password: this.state.password,
         };
         const TOKEN = await axios.post("/api/token/", formData);
         localStorage.setItem("user-token", TOKEN.data.access);
-        this.token = localStorage.getItem("user-token");
-        const USER = await axios.get("/users/", {
-          headers: {
-            Authorization: `Bearer ${this.token}`,
-          },
-        });
-        localStorage.setItem("username", USER.data.results[0].username);
-        this.$store.dispatch("userToken", this.token);
+        localStorage.setItem("username", this.state.username);
+        this.$store.dispatch("userToken", localStorage.getItem("user-token"));
         this.$router.push("/").catch(() => {});
-      } catch {
-        this.usernameError = this.username == '' ? 'Please enter a username !' : 'Invalid username !';
-        this.passwordError = this.password == '' ? 'Please enter a password !' : 'Invalid password !';
+      } catch (error) {
+         console.error(error)
         return;
       }
     },
@@ -113,3 +90,116 @@ export default {
   },
 };
 </script>
+<style scoped>
+*{
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+  font-family: sans-serif;
+  transition: 1s ease;
+}
+.body{
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgb(231, 224, 224);
+  position: absolute;
+}
+.box{
+  position: absolute;
+  width: 350px;
+  height: 500px;
+  background: none;
+  left: calc(50% - 175px);
+  top: calc(50% - 250px);
+  transform-style: preserve-3d;
+}
+.box div{
+  width: 350px;
+  height: 500px;
+  position: absolute;
+  background-image: radial-gradient(#fff, rgb(231, 224, 224));
+  box-shadow: 0px 0px 5px rgb(231, 224, 224), 0px 0px 30px rgb(231, 224, 224),
+  inset 0px 0px 5px rgb(231, 224, 224), inset 0px 0px 30px rgb(231, 224, 224);
+  border: 3px solid white;
+}
+.box div:nth-child(5){
+  height: 350px;
+  box-shadow:  0px 0px 10px rgb(231, 224, 224), 0px 0px 30px rgb(231, 224, 224);
+}
+.a{transform: rotateY(0deg) translateZ(175px);}
+.b{transform: rotateY(90deg) translateZ(175px);}
+.c{transform: rotateY(180deg) translateZ(175px);}
+.d{transform: rotateY(270deg) translateZ(175px);}
+.e{transform: rotateX(-90deg) translateZ(325px);}
+.a label{
+  position: relative;
+  width: 200px;
+  left: 72px;
+  top: 170px;
+  color: white;
+  font-size: 20px;
+  height: 50px;
+  line-height: 50px;
+  margin: 20px 0px;
+  text-align: center;
+  display: block;
+  background-image: linear-gradient(45deg, #8000ff, #ff00c4);
+  border-radius: 25px;
+  box-shadow: 0px 0px 5px #ff44f8, 0px 0px 30px #ff44f8;
+  cursor: pointer;
+}
+#sup, #btn{
+  background-image: linear-gradient(45deg, #ff00c4, #8000ff);
+  color: white;
+  cursor: pointer;
+}
+#btn:focus{
+  width: calc(100% - 80px);
+  left: 40px;
+}
+.title{
+  width: 100%;
+  text-align: center;
+  line-height: 30px;
+  font-size: 30px;
+  font-weight: bold;
+  color: #8d006c;
+  text-shadow: 0px 0px 20px #ff00c4;
+  position: absolute;
+}
+.frm input{
+  position: relative;
+  width:  calc(100% - 80px);
+  left: 40px;
+  display: block;
+  text-align: center;
+  line-height: 40px;
+  top: 60px;
+  background: none;
+  outline: none;
+  border: none;
+  border-radius: 25px;
+  box-shadow: 0px 0px 5px #ff44f8, 0px 0px 30px #ff44f8;
+  margin: 20px 0px;
+  font-size: 16px;
+}
+.frm input:focus{
+  width: calc(100% -40px);
+  left: 20px;
+  background: #ffcbf9;
+  box-shadow: 0px 0px 5px #000, 0px 0px 30px #c900b2;
+}
+#btm{
+  position: absolute;
+  bottom: 20px;
+  font-size: 20px;
+  color: #720058;
+  text-align: center;
+  width: 100%;
+  cursor: pointer;
+}
+#chk1:checked ~ .box{transform:  rotateY(-180deg);}
+#chk2:checked ~ .box{transform:  rotateY( -89.99deg);}
+</style>
